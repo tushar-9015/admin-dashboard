42230a267327db6acdc3b979ec5f609984e97730
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-05-12 20:31:08 +0530

    navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import "./navbar.scss"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className='navbar-wrapper'>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <SearchOutlinedIcon  />
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <LanguageOutlinedIcon className='icon'/>
+            English
+          </div>
+          <div className='item'>
+            <DarkModeOutlinedIcon className='icon'/>
+          </div>
+          <div className='item'>
+          <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className='item'>
+          <NotificationsNoneOutlinedIcon className="icon" />
+          </div>
+          <div className='item'>
+          <ChatBubbleOutlineOutlinedIcon className="icon" />
+          </div>
+          <div className='item'>
+          <ListOutlinedIcon className="icon" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './home.scss';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import Navbar from '../../Navbar/Navbar';
 
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar />
-      <div className='home-container'>container</div>
+      <div className='home-container'>
+        <Navbar />
+        home container
+      </div>
     </div>
   )
 }
