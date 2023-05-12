bf0e49232b63340c8dd35a8277808a8be098a171
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-05-13 00:05:47 +0530

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
       <div className='home-container'>
         <Navbar />
         <div className='widget-container'>
-          <Widget />
-          <Widget />
-          <Widget />
-          <Widget />
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
         </div>
       </div>
     </div>
