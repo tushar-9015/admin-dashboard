b7723886558d43264b40263fb1b99b9725841ac1
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-05-12 16:51:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Login from "./pages/Login/Login";
 import List from "./pages/List/List";
 import Single from "./pages/Single/Single";
 import New from "./pages/New/New";
-
-
 import {
   BrowserRouter,
   Routes,
@@ -24,11 +22,19 @@ function App() {
           <Route index element={<List />} />
           <Route path=":userId" element={<Single/>} />
           <Route path="new" element={<New />} />
-          </Route>                   
+          </Route>     
+          <Route path="products">
+          <Route index element={<List />} />
+          <Route path=":productId" element={<Single/>} />
+          <Route path="new" element={<New />} />
+          </Route>                
       </Route>                     
      </Routes>
     </BrowserRouter>
   )
 } 
+
+
+export default App
 
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './home.scss';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 const Home = () => {
   return (
     <div className='home'>
-      home
+      <Sidebar />
+      <div className='home-container'>container</div>
     </div>
   )
 }
