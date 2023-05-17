8fe1705030363e63be2a8628db464d1b2aa921d5
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-05-17 22:17:46 +0530

    commit

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './new.scss'
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Navbar from '../../components/Navbar/Navbar';
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 const New = () => {
   return (
@@ -9,7 +10,51 @@ const New = () => {
       <Sidebar />
       <div className="new-container">
         <Navbar />
-        test
+        <div className="top">
+          <h1 className="title">Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg' alt='' />
+          </div>
+          <div className="right">
+            <form>
+            <div className='form-input'>
+                <label htmlFor='file'>Image: <DriveFolderUploadOutlinedIcon className='icon' /></label>
+                <input id='file' type='file' style={{display:"none"}} />
+              </div>
+              <div className='form-input'>
+                <label>Username</label>
+                <input type='text' placeholder='john_doe' />
+              </div>
+              <div className='form-input'>
+                <label>Name and Surname</label>
+                <input type='text' placeholder='John Doe' />
+              </div>
+              <div className='form-input'>
+                <label>Email</label>
+                <input type='email' placeholder='john@gmail.com'/>
+              </div>
+              <div className='form-input'>
+                <label>Phone</label>
+                <input type='text' placeholder='123 4567 890'/>
+              </div>
+              <div className='form-input'>
+                <label>Password</label>
+                <input type='password'/>
+              </div>
+              <div className='form-input'>
+                <label>Address</label>
+                <input type='text' placeholder='b-17 saket, New Delhi'/>
+              </div>
+              <div className='form-input'>
+                <label>Country</label>
+                <input type='text' placeholder='India'/>
+              </div>
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   )
